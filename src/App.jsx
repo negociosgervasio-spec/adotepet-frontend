6350d883e15aa46222603459e95bdbc9d452db51
negociosgervasio-spec <negociosgervasio@gmail.com>
2026-08-27 07:04:51 -0300

    Updating main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,10 @@ import Footer from "./components/layout/Footer";
 import { Toaster } from "sonner";
 
 const App = () => {
-  const location = useLocation();
-
   return (
     <div className="bg-surface">
-      {location.pathname !== "404" && <Navbar />}
-      <Toaster position="top-center" duration={3000}/>
+      <Navbar />
+      <Toaster position="top-center" duration={3000} />
       <main className="container m-auto max-w-5xl min-h-screen">
         <Outlet />
       </main>
